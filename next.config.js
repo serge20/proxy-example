fe50b7eaf3d70004608d57930e1aca32e7929023
serge20 <sergebasile@gmail.com>
2022-10-03 10:05:34 -0700

    example done

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // These rewrites are checked after headers/redirects
+        // and before all files including _next/public files which
+        // allows overriding page files
+        {
+          source: "/jbn/:path*",
+          destination: "https://jellybabiesnft.com/:path*",
+        },
+      ],
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
